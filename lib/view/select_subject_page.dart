9261e8f758dac57eccfde316445049e19412a3ec
soda0408 <soooda0408@gmail.com>
2024-12-11 00:00:59 +0900

    エビちゃん追加

--- lib/view/select_subject_page.dart
+++ lib/view/select_subject_page.dart
@@ -20,157 +20,169 @@ class _SelectSubjectPageState extends State<SelectSubjectPage> {
         backgroundColor: AppColor.brand.secondary,
         centerTitle: true,
         automaticallyImplyLeading: false,
-        toolbarHeight: deviceHeight/6,
-        leadingWidth: deviceWidth/9,
+        toolbarHeight: deviceHeight / 5,
+        leadingWidth: deviceWidth / 9,
         title: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(150),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black26,
-                    spreadRadius: 1.0,
-                    blurRadius: 1.0,
-                    offset: Offset(5,5),
-                  )
-                ],
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(150),
-                child: Image.asset(
-                'assets/images/logo.png',
-                height: deviceHeight/7,
-              ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(150),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black26,
+                spreadRadius: 1.0,
+                blurRadius: 1.0,
+                offset: Offset(3, 5),
+              )
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(150),
+            child: Image.asset(
+              'assets/images/logo.png',
+              height: deviceHeight / 6.5,
+            ),
           ),
         ),
         leading: IconButton(
             style: IconButton.styleFrom(
-              foregroundColor: AppColor.brand.secondary,
+                foregroundColor: AppColor.brand.secondary,
                 backgroundColor: AppColor.brand.primary,
-                iconSize: deviceWidth/20,
+                iconSize: deviceWidth / 20,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
-                )
-              ),
-              onPressed: (){
-                showDialog(
-                  context: context, 
-                  builder: (BuildContext context){
-                    return const AlertDialog();
-                  },
-                );
-              }, 
-              icon: const Icon(Icons.arrow_back_rounded)
-            ),
+                )),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return const AlertDialog();
+                },
+              );
+            },
+            icon: const Icon(Icons.arrow_back_rounded)),
         actions: [
-            IconButton(
+          IconButton(
               style: IconButton.styleFrom(
-                foregroundColor: AppColor.brand.secondary,
-                backgroundColor: AppColor.brand.primary,
-                iconSize: deviceWidth/20,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                )
-              ),
-              onPressed: (){
+                  foregroundColor: AppColor.brand.secondary,
+                  backgroundColor: AppColor.brand.primary,
+                  iconSize: deviceWidth / 20,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  )),
+              onPressed: () {
                 showDialog(
-                  context: context, 
-                  builder: (BuildContext context){
+                  context: context,
+                  builder: (BuildContext context) {
                     return const AlertDialog();
                   },
                 );
-              }, 
-              icon: const Icon(Icons.volume_up_rounded)
-            ),
-            Padding(padding: EdgeInsets.only(left: deviceWidth/50)),
-            IconButton(
+              },
+              icon: const Icon(Icons.volume_up_rounded)),
+          Padding(padding: EdgeInsets.only(left: deviceWidth / 90)),
+          IconButton(
               style: IconButton.styleFrom(
-                foregroundColor: AppColor.brand.secondary,
-                backgroundColor: AppColor.brand.primary,
-                iconSize: deviceWidth/20,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                )
-              ),
-              onPressed: (){
+                  foregroundColor: AppColor.brand.secondary,
+                  backgroundColor: AppColor.brand.primary,
+                  iconSize: deviceWidth / 20,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  )),
+              onPressed: () {
                 showDialog(
-                  context: context, 
-                  builder: (BuildContext context){
+                  context: context,
+                  builder: (BuildContext context) {
                     return const AlertDialog();
                   },
                 );
-              }, 
-              icon: const Icon(Icons.help_outline_rounded)
-            ),
-            Padding(padding: EdgeInsets.all(deviceWidth/100)),
-          ],
+              },
+              icon: const Icon(Icons.help_outline_rounded)),
+          Padding(padding: EdgeInsets.all(deviceWidth / 150)),
+        ],
       ),
       backgroundColor: AppColor.brand.primary,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Padding(padding: EdgeInsets.all(deviceHeight/40)),
-            OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                foregroundColor: Colors.black,
-                side: const BorderSide(
-                  color: Colors.black,
-                  width: 3,
-                ),
-                minimumSize: Size(deviceWidth/2, deviceHeight/6),
-              ),
-              onPressed: (){
+      body: Stack(
+        children: [
+          Container(
+            alignment: Alignment.bottomRight,
+            child: GestureDetector(
+              onTap: () {
                 showDialog(
-                  context: context, 
-                  builder: (BuildContext context){
+                  context: context,
+                  builder: (BuildContext context) {
                     return const AlertDialog();
                   },
                 );
               },
-              child: const Text("英語",style: TextStyle(fontSize: 40)),
-            ),
-            OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                foregroundColor: Colors.black,
-                side: const BorderSide(
-                  color: Colors.black,
-                  width: 3,
-                ),
-                minimumSize: Size(deviceWidth/2, deviceHeight/6),
+              child: Image.asset(
+                'assets/images/shrimp.png',
+                width: deviceWidth / 5.5,
               ),
-              onPressed: (){
-                showDialog(
-                  context: context, 
-                  builder: (BuildContext context){
-                    return const AlertDialog();
-                  },
-                );
-              },
-              child: const Text("漢字",
-                style: TextStyle(fontSize: 40)),
             ),
-            OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                foregroundColor: Colors.black,
-                side: const BorderSide(
-                  color: Colors.black,
-                  width: 3,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    side: const BorderSide(
+                      color: Colors.black,
+                      width: 3,
+                    ),
+                    minimumSize: Size(deviceWidth / 2, deviceHeight / 6),
+                  ),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog();
+                      },
+                    );
+                  },
+                  child: const Text("英語", style: TextStyle(fontSize: 45)),
                 ),
-                minimumSize: Size(deviceWidth/2, deviceHeight/6),
-              ),
-              onPressed: (){
-                showDialog(
-                  context: context, 
-                  builder: (BuildContext context){
-                    return const AlertDialog();
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    side: const BorderSide(
+                      color: Colors.black,
+                      width: 3,
+                    ),
+                    minimumSize: Size(deviceWidth / 2, deviceHeight / 6),
+                  ),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog();
+                      },
+                    );
                   },
-                );
-              },
-              child: const Text("ドイツ語",style: TextStyle(fontSize: 40)),
+                  child: const Text("漢字", style: TextStyle(fontSize: 45)),
+                ),
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    side: const BorderSide(
+                      color: Colors.black,
+                      width: 3,
+                    ),
+                    minimumSize: Size(deviceWidth / 2, deviceHeight / 6),
+                  ),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog();
+                      },
+                    );
+                  },
+                  child: const Text("ドイツ語", style: TextStyle(fontSize: 45)),
+                ),
+              ],
             ),
-            Padding(padding: EdgeInsets.all(deviceHeight/40)),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
