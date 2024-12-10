aae912c1f0c4e8d9f475326d594070bd4c525e04
soda0408 <soooda0408@gmail.com>
2024-12-11 08:58:24 +0900

    クレジット追加、中身は後で！

--- lib/view/start_page.dart
+++ lib/view/start_page.dart
@@ -22,106 +22,129 @@ class _StartPageState extends State<StartPage> {
           top: deviceHeight / 13,
           right: deviceWidth / 6,
           left: deviceWidth / 6,
-          bottom: deviceHeight / 13,
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(150),
-                boxShadow: const [
-                  BoxShadow(
-                    blurRadius: 1.0,
-                    color: Colors.black38,
-                    spreadRadius: 1.0,
-                    offset: Offset(10, 10),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(150),
+                      boxShadow: const [
+                        BoxShadow(
+                          blurRadius: 1.0,
+                          color: Colors.black38,
+                          spreadRadius: 1.0,
+                          offset: Offset(10, 10),
+                        ),
+                      ],
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(150),
+                      child: Image.asset(
+                        'assets/images/logo.png',
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(
+                        'assets/images/shrimp.png',
+                        width: deviceWidth / 7,
+                      ),
+                      Image.asset(
+                        'assets/images/shrimp.png',
+                        width: deviceWidth / 7,
+                      ),
+                      Image.asset(
+                        'assets/images/shrimp.png',
+                        width: deviceWidth / 7,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        style: IconButton.styleFrom(
+                            foregroundColor: AppColor.brand.logo,
+                            backgroundColor: AppColor.brand.primary,
+                            iconSize: deviceWidth / 18,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            )),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return const AlertDialog();
+                            },
+                          );
+                        },
+                        icon: const Icon(Icons.volume_up_rounded),
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          fixedSize: Size(deviceWidth / 5, deviceHeight / 9),
+                          foregroundColor: AppColor.brand.logo,
+                          backgroundColor: AppColor.brand.primary,
+                          textStyle: TextStyle(
+                            fontSize: deviceWidth / 23,
+                          ),
+                        ),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return const AlertDialog();
+                            },
+                          );
+                        },
+                        child: const Text("START"),
+                      ),
+                      IconButton(
+                        style: IconButton.styleFrom(
+                            foregroundColor: AppColor.brand.logo,
+                            backgroundColor: AppColor.brand.primary,
+                            iconSize: deviceWidth / 18,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            )),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return const AlertDialog();
+                            },
+                          );
+                        },
+                        icon: const Icon(Icons.exit_to_app_rounded),
+                      ),
+                    ],
                   ),
                 ],
               ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(150),
-                child: Image.asset(
-                  'assets/images/logo.png',
-                ),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                fixedSize: Size(deviceWidth / 5, deviceHeight / 9),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.asset(
-                  'assets/images/shrimp.png',
-                  width: deviceWidth / 7,
-                ),
-                Image.asset(
-                  'assets/images/shrimp.png',
-                  width: deviceWidth / 7,
-                ),
-                Image.asset(
-                  'assets/images/shrimp.png',
-                  width: deviceWidth / 7,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  style: IconButton.styleFrom(
-                      foregroundColor: AppColor.brand.secondary,
-                      backgroundColor: AppColor.brand.primary,
-                      iconSize: deviceWidth / 18,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      )),
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return const AlertDialog();
-                      },
-                    );
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return const AlertDialog();
                   },
-                  icon: const Icon(Icons.volume_up_rounded),
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    fixedSize: Size(deviceWidth / 5, deviceHeight / 9),
-                    foregroundColor: AppColor.brand.secondary,
-                    backgroundColor: AppColor.brand.primary,
-                    textStyle: TextStyle(
-                      fontSize: deviceWidth / 23,
-                    ),
-                  ),
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return const AlertDialog();
-                      },
-                    );
-                  },
-                  child: const Text("START"),
-                ),
-                IconButton(
-                  style: IconButton.styleFrom(
-                      foregroundColor: AppColor.brand.secondary,
-                      backgroundColor: AppColor.brand.primary,
-                      iconSize: deviceWidth / 18,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      )),
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return const AlertDialog();
-                      },
-                    );
-                  },
-                  icon: const Icon(Icons.exit_to_app_rounded),
-                ),
-              ],
+                );
+              },
+              child: Text(
+                "＠ガリバタコーン",
+                style: TextStyle(
+                    color: Colors.white54, fontSize: deviceWidth / 70),
+              ),
             ),
           ],
         ),
