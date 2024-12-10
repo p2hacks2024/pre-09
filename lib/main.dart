a25aaf8d21b1e2bab771bb3cdcd61e50f9d0fdb2
soda0408 <soooda0408@gmail.com>
2024-12-10 16:36:31 +0900

    デモのバナーを消した

--- lib/main.dart
+++ lib/main.dart
@@ -29,9 +29,11 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
+        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       home: const SelectSubjectPage(),
     );
   }
