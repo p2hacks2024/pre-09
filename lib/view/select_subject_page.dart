d0137e02cfbda591c2e1e22aa06e3c8d5b1d621b
Ruri-Kinoshita <b1023081@fun.ac.jp>
2024-12-13 21:22:24 +0900

    appbarのwidget化

--- lib/view/select_subject_page.dart
+++ lib/view/select_subject_page.dart
@@ -1,3 +1,5 @@
+import 'package:ebidence/viewmodel/ebidence_appbar.dart';
+
 import 'package:flutter/material.dart';
 
 import 'package:ebidence/constant/app_color.dart';
@@ -16,88 +18,9 @@ class _SelectSubjectPageState extends State<SelectSubjectPage> {
     final double deviceWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColor.brand.secondary,
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-        toolbarHeight: deviceHeight / 5,
-        leadingWidth: deviceWidth / 9,
-        title: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(150),
-            boxShadow: const [
-              BoxShadow(
-                color: Colors.black26,
-                spreadRadius: 1.0,
-                blurRadius: 1.0,
-                offset: Offset(3, 5),
-              )
-            ],
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(150),
-            child: Image.asset(
-              'assets/images/logo.png',
-              height: deviceHeight / 6.5,
-            ),
-          ),
-        ),
-        leading: IconButton(
-            style: IconButton.styleFrom(
-                foregroundColor: AppColor.brand.secondary,
-                backgroundColor: AppColor.brand.primary,
-                iconSize: deviceWidth / 20,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                )),
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return const AlertDialog();
-                },
-              );
-            },
-            icon: const Icon(Icons.arrow_back_rounded)),
-        actions: [
-          IconButton(
-              style: IconButton.styleFrom(
-                  foregroundColor: AppColor.brand.secondary,
-                  backgroundColor: AppColor.brand.primary,
-                  iconSize: deviceWidth / 20,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  )),
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return const AlertDialog();
-                  },
-                );
-              },
-              icon: const Icon(Icons.volume_up_rounded)),
-          Padding(padding: EdgeInsets.only(left: deviceWidth / 90)),
-          IconButton(
-              style: IconButton.styleFrom(
-                  foregroundColor: AppColor.brand.secondary,
-                  backgroundColor: AppColor.brand.primary,
-                  iconSize: deviceWidth / 20,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  )),
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return const AlertDialog();
-                  },
-                );
-              },
-              icon: const Icon(Icons.help_outline_rounded)),
-          Padding(padding: EdgeInsets.all(deviceWidth / 150)),
-        ],
-      ),
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(deviceHeight / 5),
+          child: const EbidenceAppbar()),
       backgroundColor: AppColor.brand.primary,
       body: Stack(
         children: [
---
+++ lib/viewmodel/ebidence_appbar.dart
@@ -0,0 +1,83 @@
+import 'package:ebidence/constant/app_color.dart';
+import 'package:flutter/material.dart';
+
+class EbidenceAppbar extends StatelessWidget {
+  const EbidenceAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double deviceHeight = MediaQuery.of(context).size.height;
+    final buttonStyle = IconButton.styleFrom(
+        foregroundColor: AppColor.brand.secondary,
+        backgroundColor: AppColor.brand.primary,
+        iconSize: deviceWidth / 20,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ));
+    return AppBar(
+      backgroundColor: AppColor.brand.secondary,
+      centerTitle: true,
+      automaticallyImplyLeading: false,
+      toolbarHeight: deviceHeight / 5,
+      leadingWidth: deviceWidth / 9,
+      title: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(150),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black26,
+              spreadRadius: 1.0,
+              blurRadius: 1.0,
+              offset: Offset(3, 5),
+            )
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(150),
+          child: Image.asset(
+            'assets/images/logo.png',
+            height: deviceHeight / 6.5,
+          ),
+        ),
+      ),
+      leading: IconButton(
+          style: buttonStyle,
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return const AlertDialog();
+              },
+            );
+          },
+          icon: const Icon(Icons.arrow_back_rounded)),
+      actions: [
+        IconButton(
+            style: buttonStyle,
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return const AlertDialog();
+                },
+              );
+            },
+            icon: const Icon(Icons.volume_up_rounded)),
+        Padding(padding: EdgeInsets.only(left: deviceWidth / 90)),
+        IconButton(
+            style: buttonStyle,
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return const AlertDialog();
+                },
+              );
+            },
+            icon: const Icon(Icons.help_outline_rounded)),
+        Padding(padding: EdgeInsets.all(deviceWidth / 150)),
+      ],
+    );
+  }
+}
