c020ea8afc66b75259ff903b85b478cd2d06bf67
Ruri-Kinoshita <b1023081@fun.ac.jp>
2024-12-13 12:48:44 +0900

    正誤を保持するプロバイダーの導入

--- lib/viewmodel/result.dart
+++ lib/viewmodel/result.dart
@@ -1,10 +1,14 @@
+import 'package:ebidence/provider/quiz_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ResultPage1 extends StatelessWidget {
+class ResultPage1 extends ConsumerWidget {
   const ResultPage1({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final quizResults = ref.watch(quizResultProvider);
+    debugPrint('クイズ結果: $quizResults');
     return Scaffold(
       appBar: AppBar(
         title: const Text('結果'),
